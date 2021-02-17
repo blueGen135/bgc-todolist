ee2d1d5b3bc6baa54f2cc91539afedb260d041a8
blueGen135 <abap.5135@hotmail.com>
2021-02-18 00:40:35 +0530

    Setup Model/Controller
    Vue js installed

---
+++ resources/js/components/app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todoContainer">
+      <div class="heading">
+          <h2 id="title">Todo List</h2>
+          <add-item-form></add-item-form>
+      </div>
+        <list-view></list-view>
+  </div>
+</template>
+<script>
+
+import addItemForm from './addItemForm'
+import listView from './listView';
+import ListView from './listView.vue';
+
+export default {
+  components:{
+      addItemForm,
+       listView
+    
+  }
+};
+</script>
+<style scoped>
+.todoContainer{
+    width:350px;
+    margin:auto;
+}
+
+</style>
